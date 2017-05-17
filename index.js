9d3f90e2c76c0285ce1788155067e0c0466368aa
paulrcook <paul@lougeek.com>
2017-05-17 16:00:37 -0400

    Added in the fetching of a geocoded location from google

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-let lat, lon, weather;
+let lat, lon, weather, locality;
 const key = '2c9b6c5080e338af6c6a2f49707dd592';
-const corsAnywhere =  'https://cors-anywhere.herokuapp.com';
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com';
 const apiUrl = 'https://api.darksky.net/forecast';
+
+let icons = new Skycons({'color': 'black'});
+icons.set({
+    "clear-day": Skycons.CLEAR_DAY,
+    "clear-night": Skycons.CLEAR_NIGHT,
+    "partly-cloudy-day": Skycons.PARTLY_CLOUDY_DAY,
+    "partly-cloudy-night": Skycons.PARTLY_CLOUDY_NIGHT,
+    "cloudy": Skycons.CLOUDY,
+    "rain": Skycons.RAIN,
+    "sleet": Skycons.SLEET,
+    "snow": Skycons.SNOW,
+    "wind": Skycons.WIND,
+    "fog": Skycons.FOG
+});
+icons.play();
 
 navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
+    let geocode = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
     fetch(`${corsAnywhere}/${apiUrl}/${key}/${lat},${lon}`, {method: 'get'})
-        .then((response) => response.json().then((data) => {
-            weather = data;
-            console.log(weather);
-            document.getElementById('location').innerHTML = weather.timezone;
-            document.getElementById('temperature').innerHTML = Math.round(weather.currently.apparentTemperature);
-            document.getElementById('weather').innerHTML = weather.currently.summary;
-            document.getElementById('icon').innerHTML = weather.currently.icon;
-        }))
+        .then((response) => response.json()
+            .then((data) => {
+                weather = data;
+                console.log(weather);
+                document.getElementById('temperature').innerHTML = Math.round(weather.currently.apparentTemperature);
+                document.getElementById('weather').innerHTML = weather.currently.summary;
+                icons.add('icon', weather.currently.icon);
+            }))
         .catch((error) => console.error(error));
+    fetch(geocode)
+        .then((response) => response.json()
+            .then((data) => {
+                locality = data;
+                console.log(locality.results);
+                document.getElementById('location').innerHTML = `${locality.results[0].address_components[2].long_name}, ${locality.results[0].address_components[4].long_name}, ${locality.results[0].address_components[5].short_name}`;
+            }));
 });
+
